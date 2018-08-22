159a93944579163b7a07a44e2167ac194a9c013d
javigoncompte <javiergonzalez7@gmail.com>
2018-08-22 09:59:56 -0500

    added files to start github repo

---
+++ ScoreKeeper.py
@@ -0,0 +1,19 @@
+#take two inputs
+#have a parser dictionary
+# 20x1 20x2 20x3 acceptablae inputs
+#if statement when a number is closed
+#if statement when the opposite number is closed as well
+#Statistics of numbers of Triple Throwed
+
+import numpy as np
+
+
+cricketNumbers = {
+    "15": 15,
+    "16": 16,
+    "17": 17,
+    "18": 18,
+    "19": 19,
+    "20": 20,
+    "bull": 25
+}
