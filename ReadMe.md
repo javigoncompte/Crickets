a4f0d927ff78553e52b593f4cb31f9430d4033c4
Javier Gonzalez-Compte <javiergonzalez7@gmail.com>
2018-09-09 19:49:05 -0500

    proper documentation added

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,8 @@
 # Crickets ScoreKeeper
 Python 3.6.5
+
+Data was stored using Pickle into disk due to the serialization of it with python
+	Since the Data is not very demanding or big and it keeps states of the objects 
 # System settings
 Cricket number x 3(2)(1) without spaces
 
@@ -23,6 +26,29 @@ Player_One = {
         "score": 0,
         "name": 'player_one'} 
 ```
+Another important system configuration is the implementation of set rounds to 20.
+I consider 20 to be a normal number since it can't take non Cricket Numbers and 20 would be enough to have a closed game.
+If evidence shows that the gamerounds are too little a simple code change in gameRound can be done.
     
 # Run the python application on terminal
+
+
+# Manual Tests Done
+
+## Test if player numbers are closed for scoring
+Set one of the players numbers to 20 and hit on the opposite players a 3x and plus 1x
+will show closed number and no points
+
+## Test if score is being calculated after the number is equal to 3
+
+Set one of the Cricket numbers to 3 and input a 1x or 2x or 3x to see if score changed
+As well hit a 3x on a number with 1 or 2 to see if number just adds up to 3
+
+## Test if a unknown number is inputed
+
+Input of a non Cricket number
+
+
+
+
 
--- ScoreKeeper.py
+++ ScoreKeeper.py
@@ -1,5 +1,15 @@
+#Pickle is going to be used to store Dictionaries into pickle files in disk
+import pickle 
+
 
 def hit(player, CricketNumber, mult):
+    #Takes in the player
+    # CricketNumber represents the number that was hit on the board
+    # mult represents the triple, double, single
+
+    # This function will provide if the number is in the player board
+    # verification of the hits is less than 3 or 3 will update hits on each Cricket number
+
     counter = mult
     if CricketNumber in  player:
         hits = player[CricketNumber]
@@ -10,6 +20,11 @@ def hit(player, CricketNumber, mult):
             player[CricketNumber] = 3
 
 def checkifclosed(players, CricketNumber):
+    #Function that will check if oppposite player has a closed Cricket number
+    #Takes in a list of players and Circket Number
+    # If player 1 hits a 3x and player two has 3 both players will show number closed
+    # When player 1 hits 1x it will show number is closed and return True
+
     player1 = players[0]
     player2 = players[1]
     if player1[CricketNumber] and player2[CricketNumber] == 3:
@@ -18,6 +33,11 @@ def checkifclosed(players, CricketNumber):
     
 
 def scores(player, CricketNumber, mult):
+    #Function will take in player, Cricket Number and (1,2,3) = multiplier  variables 
+    # The first logic will test if the players Cricket number has 3 hits so scoring can be allowed
+    # if score does not equal 0 it will add score by getting current score and multiplying plus original score
+    #  if score does not equal 0 it will make the math by getting the Cricket number * the multiplier
+
   if player[str(CricketNumber)] == 3:
       score = player['score']
       if score != 0:
@@ -26,13 +46,16 @@ def scores(player, CricketNumber, mult):
       else:
         player['score'] = int(CricketNumber) * int(mult)
       
-def showStatus(player): #SHOW player board
+def showStatus(player): #SHOW player board and save player board
+    #This function returns the board and saves the board after every action
+
         return(player)
 
 
-#input being 20 x1(single double or tripple)
 def runCricket():
+    #input being 20 x1(single double or tripple)
     #25 will be the bullseye
+    # First we will define the player boards
     Player_One = {
         "15": 0,
         "16": 0,
@@ -40,7 +63,7 @@ def runCricket():
         "18": 0,
         "19": 0,
         "20": 0,
-        "25": 0,
+        "25": 0, #bullseye
         "score": 0,
         "name": 'player_one'
     }
@@ -53,27 +76,37 @@ def runCricket():
         "18": 0,
         "19": 0,
         "20": 0,
-        "25": 0,
+        "25": 0, #bullseye
         "score": 0,
         'name': 'player_2'
     }
 
-    turnTitle = ["1st", "2nd", "3rd"]
+    turnTitle = ["1st", "2nd", "3rd"] #Turntitles are the number of turns each player has 
 
-    Players = [Player_One, Player_Two]
-    for gameRound in range(20):
-        for player in Players:
+    Players = [Player_One, Player_Two] # Set Players by making a list of Player1 and Player2
+    for gameRound in range(20): #The start of the game which is limited by 20(talked in the ReadMe as to why)
+        for player in Players: 
+        # For each player we will do the following steps
             print(player['name'] + " : Throw your darts!")
             print(" (ex) triple of 20 #=> 20x3 ")
             print(" (ex) inner bull #=> 25x2 ")
             print(" (ex) out of Cricket-Numbers #=> ")
             print(" (ex) Quit the Game #=> quit ")
-            for i in turnTitle:
+
+
+            for i in turnTitle: 
+            # for each Turn we will do
                 print("player [" + player['name'] + "] " + i + " dart of round" +
                       str(gameRound + 1) + " : ", end="")
 
-                while True:
+                while True: #If the While breaks it will will go to next round! or if quit returns false
+
                     try:
+                        #First Test will be to verify the player wants to quit the most unprobable one
+                        #Second Step will check if Cricket Number is Closed
+                        #Third will do a score
+                        # Give exceptions when user input error or invalid input 
+                        
                         res = input()
                         
                         if res == "quit":
